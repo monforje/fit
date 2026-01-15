3c00f4ca86883fbb8d92c946b1465c7b5f77585a
monforje <daniil234agentfinger.monforje@gmail.com>
2026-01-15 17:02:48 +1000

    alembic revision -m "create activity table" __pycache__/

---
+++ alembic/versions/6249b081258a_create_activity_table.py
@@ -0,0 +1,28 @@
+"""create activity table
+
+Revision ID: 6249b081258a
+Revises: 
+Create Date: 2026-01-15 17:00:47.031807
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '6249b081258a'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass

03c8693a8263c586b0f13f009596866e16a50ad7
monforje <daniil234agentfinger.monforje@gmail.com>
2026-01-15 17:11:15 +1000

    немного починил env.py чтобы брал DATABASE_URL из .env файла

--- alembic/env.py
+++ alembic/env.py
@@ -1,10 +1,14 @@
 from logging.config import fileConfig
 import os
+from dotenv import load_dotenv
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+
+# load environment variables from .env file
+load_dotenv()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
